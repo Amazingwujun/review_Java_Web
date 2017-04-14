755514f62262ea5c98ee281dc89d0dec38c4b5ae
Amazingwujun <85998282@qq.com>
2017-04-14 14:31:37 +0800

    Update DoFormServlet.java

--- src/main/java/com/cnblogs/lesson_thirty/DoFormServlet.java
+++ src/main/java/com/cnblogs/lesson_thirty/DoFormServlet.java
@@ -33,7 +33,6 @@ public class DoFormServlet extends HttpServlet {
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
