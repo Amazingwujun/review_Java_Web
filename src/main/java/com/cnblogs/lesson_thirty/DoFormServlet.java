0975080879e904c07e6bddb8d6969adfe4378256
Amazingwujun <85998282@qq.com>
2017-04-14 14:38:25 +0800

    jj

---
+++ src/main/java/com/cnblogs/lesson_thirty/DoFormServlet.java
@@ -0,0 +1,51 @@
+package com.cnblogs.lesson_thirty;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet/DoFormServlet")
+public class DoFormServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("content-type", "text/html;charset=UTF-8");
+		req.setCharacterEncoding("utf8");
+		
+
+		PrintWriter out = resp.getWriter();
+		
+		System.out.println();
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		if (Token == null || !Token.equals((String) req.getParameter("Token"))) {
+			System.err.println("请勿重复提交请求");
+			out.write("数据提交错误");
+			return;
+		}
+
+		System.out.println(name);
+	
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
